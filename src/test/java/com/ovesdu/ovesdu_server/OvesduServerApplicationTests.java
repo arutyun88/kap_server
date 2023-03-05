5840428ba32fc88bfb3a0e977ffdece6121032d5
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-03-06 01:30:45 +0400

    project init

---
+++ src/test/java/com/ovesdu/ovesdu_server/OvesduServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovesdu.ovesdu_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvesduServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
